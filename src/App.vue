5f8964e6ac637403159e688360451b3247e65d83
Nhat12072002 <104749768+Nhat12072002@users.noreply.github.com>
2023-11-27 20:17:09 +0700

    Passing data to Components

--- src/App.vue
+++ src/App.vue
@@ -1,54 +1,31 @@
 <!-- App.vue -->
 <template>
   <div id="app">
-    <TabContainer>
-      <!-- Named Slot: Header -->
-      <template v-slot:header>
-        <h1>Tab Example</h1>
-      </template>
-
-      <!-- Compilation Scope (Single Slot): -->
-      <template v-slot="{ tabIndex }">
-        <div v-if="tabIndex === 0">
-          <p>Content for Tab 1</p>
-        </div>
-        <div v-else-if="tabIndex === 1">
-          <p>Content for Tab 2</p>
-        </div>
-        <div v-else-if="tabIndex === 2">
-          <p>Content for Tab 3</p>
-        </div>
-        <div v-else-if="tabIndex === 3">
-          <p>Content for Tab 4</p>
-        </div>
-      </template>
-
-      <!-- Named Slot: Footer (Scoped Slot) -->
-      <template v-slot:footer="{ message }">
-        <p>{{ message }}</p>
-      </template>
-    </TabContainer>
+    <ParentComponent />
+    <AnotherChildComponent />
   </div>
 </template>
 
 <script>
-import TabContainer from './components/TabContainer.vue';
+import ParentComponent from './components/ParentComponent.vue';
+import AnotherChildComponent from './components/AnotherChildComponent.vue';
+import { createApp } from 'vue';
+
+
 
 export default {
-  name: 'App',
   components: {
-    TabContainer
-  }
+    ParentComponent,
+    
+  },
 };
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
   text-align: center;
-  color: #020202;
-  margin-top: 60px;
+  color: #2c3e50;
+  margin-top: 20px;
 }
-
-/* Add your global styles here */
 </style>
---
+++ src/components/ParentComponent.vue
@@ -0,0 +1,46 @@
+<!-- ParentComponent.vue -->
+<template>
+  <div class="container">
+    <h1>Component Cha</h1>
+    <p>Thông điệp từ cha: {{ thongDiepCha }}</p>
+    <p>Thông điệp từ Component Con: {{ thongDiepCon }}</p>
+    <ChildComponent :thongDiep="thongDiepCha" @guiSuKien="nhanSuKienTuCon" />
+    <AnotherChildComponent @suKienKhac="nhanSuKienTuConKhac"></AnotherChildComponent>
+  </div>
+</template>
+
+<script>
+import ChildComponent from './ChildComponent.vue';
+import AnotherChildComponent from './AnotherChildComponent.vue';
+export default {
+  data() {
+    return {
+      thongDiepCha: 'Xin chào từ Component Cha!',
+      thongDiepCon: '',
+    };
+  },
+  methods: {
+    nhanSuKienTuCon(thongDiep) {
+      this.thongDiepCon = thongDiep;
+    },
+    nhanSuKienTuConKhac(thongDiep) {
+      this.thongDiepCon = thongDiep;
+    },
+  },
+  components: {
+    ChildComponent,
+    AnotherChildComponent,
+  },
+};
+</script>
+
+<style scoped>
+.container {
+  max-width: 600px;
+  margin: 20px auto;
+  padding: 20px;
+  border: 1px solid #ddd;
+  border-radius: 8px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+}
+</style>
